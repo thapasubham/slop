0764a27993c8714e1967dd10e1d8beb76a9e91d8
Subham Thapa <subhamits4.2@gmail.com>
2026-03-16 14:24:01 +0545

    brr

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-static';
-import adapter from 'amplify-adapter';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from 'amplify-adapter';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
